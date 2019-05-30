3ae3c254edf310a45a4394a9c536c77c6104bfda
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 20:29:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,11 @@
 def my_each(array)
-  if array.length == 0
-    "This block should not run!"
-  else
+  if array.length != 0
     i = 0
 
     while i < array.length
       yield[i]
 
-      i =+ 1
+      i = i + 1
     end
   end
 end
