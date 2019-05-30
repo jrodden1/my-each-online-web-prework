2811bbb597e0b053903edda10a9e51d6695256c5
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 20:46:50 +0000

    Automatically backed up by Learn

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 require 'pry'
 
 describe "my_each" do
-  file = File.read('./my_each.rb')
+  file = File.read('../my_each.rb')
 
   it "does not call on .each" do
     expect(file).to_not include(".each")
