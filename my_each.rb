b890df6604596b851bb762ef20a26b0bfe1752f5
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 20:56:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,12 @@
-require 'pry'
+
 def my_each(words)
   if block_given?
     i = 0
 
     while i < words.length
-      binding.pry
       yield words[i]
-      binding.pry
       i = i + 1
     end
-
-    binding.pry
   end
   words
 end
