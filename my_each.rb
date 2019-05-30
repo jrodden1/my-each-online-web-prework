647a3114aa944852faa650eafbe4b5d6fa4cd11e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-30 20:35:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
-def my_each(array) { |word| puts word}
-  if array.length != 0
+def my_each(array) 
+  if block_given?  
     i = 0
 
     while i < array.length
       yield[i]
-
       i = i + 1
+      array
     end
   end
 end
